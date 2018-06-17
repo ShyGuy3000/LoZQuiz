6b49131527983f8e3991714c435c7c36191d33fc
Kevin B <38273676+ShyGuy3000@users.noreply.github.com>
2018-06-17 18:34:05 -0400

    Update #1
    Updates were made to accomodate some changes needed for my Android nanodegree course.
    -Added descriptions for View types in MainActivity.java
    -Added an if/else statement at the bottom for the toast message.

--- app/src/main/java/com/example/android/myquiz/MainActivity.java
+++ app/src/main/java/com/example/android/myquiz/MainActivity.java
@@ -31,72 +31,80 @@ public class MainActivity extends AppCompatActivity {
 
 
     /**
-     * This method is called when the 'Take This!' button is clicked to compile the quiz results     *
+     * This method is called when the 'Take This!' button is clicked to compile the quiz results
      * A local variable (quizScore) is used to hold the score value.
+     * The Views use a naming convention appended with two letters to indicate the type of view
+     *      rb = RadioButton
+     *      cb = CheckBox
+     *      et = EditText
      */
     public void quizResults(View view) {
 
         int quizScore = 0;
 
         //The correct response for question 1 is Zelda's Lullaby (C).
-        RadioButton answerQuestionOne = (RadioButton) findViewById(R.id.question1_AnswerC);
+        RadioButton answerQuestionOne = (RadioButton) findViewById(R.id.question1_AnswerC_rb);
         boolean questionOneAnswerC = answerQuestionOne.isChecked();
         if (questionOneAnswerC) {
             quizScore += 1;
         }
 
         //These are the correct responses for question 2: A, D, F
-        CheckBox answerQuestionTwoCheckboxA = (CheckBox) findViewById(R.id.question2_AnswerA);
+        CheckBox answerQuestionTwoCheckboxA = (CheckBox) findViewById(R.id.question2_AnswerA_cb);
         boolean questionTwoAnswerA = answerQuestionTwoCheckboxA.isChecked();
-        CheckBox answerQuestionTwoCheckboxB = (CheckBox) findViewById(R.id.question2_AnswerB);
+        CheckBox answerQuestionTwoCheckboxB = (CheckBox) findViewById(R.id.question2_AnswerB_cb);
         boolean questionTwoAnswerB = answerQuestionTwoCheckboxB.isChecked();
-        CheckBox answerQuestionTwoCheckboxC = (CheckBox) findViewById(R.id.question2_AnswerC);
+        CheckBox answerQuestionTwoCheckboxC = (CheckBox) findViewById(R.id.question2_AnswerC_cb);
         boolean questionTwoAnswerC = answerQuestionTwoCheckboxC.isChecked();
-        CheckBox answerQuestionTwoCheckboxD = (CheckBox) findViewById(R.id.question2_AnswerD);
+        CheckBox answerQuestionTwoCheckboxD = (CheckBox) findViewById(R.id.question2_AnswerD_cb);
         boolean questionTwoAnswerD = answerQuestionTwoCheckboxD.isChecked();
-        CheckBox answerQuestionTwoCheckboxE = (CheckBox) findViewById(R.id.question2_AnswerE);
+        CheckBox answerQuestionTwoCheckboxE = (CheckBox) findViewById(R.id.question2_AnswerE_cb);
         boolean questionTwoAnswerE = answerQuestionTwoCheckboxE.isChecked();
-        CheckBox answerQuestionTwoCheckboxF = (CheckBox) findViewById(R.id.question2_AnswerF);
+        CheckBox answerQuestionTwoCheckboxF = (CheckBox) findViewById(R.id.question2_AnswerF_cb);
         boolean questionTwoAnswerF = answerQuestionTwoCheckboxF.isChecked();
         if (questionTwoAnswerA && !questionTwoAnswerB && !questionTwoAnswerC && questionTwoAnswerD && !questionTwoAnswerE && questionTwoAnswerF) {
             quizScore += 1;
         }
 
         //The correct response for question 3 is 50 (C).
-        RadioButton answerQuestionThree = (RadioButton) findViewById(R.id.question3_AnswerC);
+        RadioButton answerQuestionThree = (RadioButton) findViewById(R.id.question3_AnswerC_rb);
         boolean questionThreeAnswerC = answerQuestionThree.isChecked();
         if (questionThreeAnswerC) {
             quizScore += 1;
         }
 
         //The correct response for question 4 is Courage (B).
-        RadioButton answerQuestionFour = (RadioButton) findViewById(R.id.question4_AnswerB);
+        RadioButton answerQuestionFour = (RadioButton) findViewById(R.id.question4_AnswerB_rb);
         boolean questionFourAnswerB = answerQuestionFour.isChecked();
         if (questionFourAnswerB) {
             quizScore += 1;
         }
 
         //The correct response for question 5 is 8 (D).
-        RadioButton answerQuestionFive = (RadioButton) findViewById(R.id.question5_AnswerD);
+        RadioButton answerQuestionFive = (RadioButton) findViewById(R.id.question5_AnswerD_rb);
         boolean questionFiveAnswerD = answerQuestionFive.isChecked();
         if (questionFiveAnswerD) {
             quizScore += 1;
         }
 
         //The correct response for question 6 is Octorok (A).
-        RadioButton answerQuestionSix = (RadioButton) findViewById(R.id.question6_AnswerA);
+        RadioButton answerQuestionSix = (RadioButton) findViewById(R.id.question6_AnswerA_rb);
         boolean questionSixAnswerA = answerQuestionSix.isChecked();
         if (questionSixAnswerA) {
             quizScore += 1;
         }
 
         //The correct response for question 7 is 'Cucco'
-        EditText questionSevenEditText = (EditText) findViewById(R.id.question7_AnswerA);
+        EditText questionSevenEditText = (EditText) findViewById(R.id.question7_AnswerA_et);
         String color = questionSevenEditText.getText().toString();
         if (color.equalsIgnoreCase("cucco")) {
             quizScore += 1;
         }
 
-        Toast.makeText(this, "You answered " + quizScore + " correctly out 7!  Thanks for playing!", Toast.LENGTH_SHORT).show();
+        if (quizScore == 7){
+            Toast.makeText(this, "You answered all " + quizScore + " correctly!?  \nYou must be the Hero of Time!", Toast.LENGTH_SHORT).show();
+        } else{
+            Toast.makeText(this, "You answered " + quizScore + " correctly out 7!  \nThanks for playing!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
